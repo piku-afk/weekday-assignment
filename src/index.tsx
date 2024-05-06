0392c905b06263be02908fccafed3a26220bd6b0
Piyush Mahato <piyush@nimbbl.biz>
2024-05-06 09:36:17 +0530

    feat: setup material ui

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,16 @@
+import { ThemeProvider } from "@mui/material";
+import CssBaseline from "@mui/material/CssBaseline";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { theme } from "./theme";
 
 import "./global.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>Hello World</h1>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <h1>Hello World</h1>
+    </ThemeProvider>
   </React.StrictMode>,
 );
